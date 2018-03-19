4999e082d7d06ad9f14839f908fa51d04c20398c
Matt Shields <mattboston@users.noreply.github.com>
2018-03-19 11:20:35 -0400

    Update check_chrony.sh

--- check_chrony.sh
+++ check_chrony.sh
@@ -3,18 +3,20 @@
 #Date:06/04/2016
 #Author: Matt Shields (https://github.com/mattboston/check_chrony) - Modified to use with both Systemd and sysvinit
 #Date:2018-03-19
-#Purpose:check the chrony service if its running or not ,if running them it will check if the Leap status is in normal status or not.if not this means that you have to check the connectivity between your server and NTP server.if all the previous is working well,the script will complare between local machine time and NTP time and give the nagios status accordingly.
+#Purpose: Check the chrony service if its running or not.  If running, then it will check if the Leap status is in normal status or not.
+# If not this means that you have to check the connectivity between your server and NTP server. If all the previous is working well,
+# the script will compare between local machine time and NTP time and give the nagios status accordingly.
 
 if [[ -z $1 ]] || [[ -z $2 ]]
 then
-  echo 'Please provide warning and critical values (in Millisecond). The first value is warning and the second is critical.'
-  exit
+  echo 'UNKNOWN - Please provide warning/critical values (in Millisecond).'
+  exit 3
 fi
 
 NUMBER_VALIDATION='^[0-9]+$'
 if ! [[ $1 =~ $NUMBER_VALIDATION ]] || ! [[ $2 =~ $NUMBER_VALIDATION ]]
 then
-  echo 'Please provide only number in the warining and critical values'
+  echo 'UNKNOWN - Please provide only number in the warning and critical values'
   exit 3
 fi
 
@@ -32,17 +34,17 @@ then
 else
   CHECK_SERVER_SYNC=`chronyc tracking | grep 'Leap status' | awk -F ':' '{print $2}' | sed -e 's/^ //'`
   if [[ $CHECK_SERVER_SYNC == 'Not synchronised' ]]
-  then 
+  then
     echo  'CRITICAL - Server is not synchronised with the ntp server'
     exit 2
-    else 
+    else
     CHECK_TIME_DIFF=`chronyc tracking | grep 'System time' | awk -F ':' '{print $2}' |awk '{print $1}'| sed -e 's/^ //'`
     CHECK_TIME_DIFF_INT=`chronyc tracking | grep 'System time' | awk -F ':' '{print $2}' |awk '{print $1}'| sed -e 's/^ //' | awk -F '.' '{print $1}'`
     DIFF_IN_SECOND=`echo "(($CHECK_TIME_DIFF * 1000))" | bc`
     FAST_SLOW_VALUE=`chronyc tracking | grep 'System time' | awk -F ':' '{print $2}' |awk '{print $3}'| sed -e 's/^ //'`
       if [[ $CHECK_TIME_DIFF_INT -gt $1  ]] && [[ $CHECK_TIME_DIFF_INT -lt $2  ]]
       then
-        echo "WARINING time is $CHECK_TIME_DIFF_INT $FAST_SLOW_VALUE of NTP Time"
+        echo "WARNING - time is $CHECK_TIME_DIFF_INT $FAST_SLOW_VALUE of NTP Time"
         echo "|Time Differences in=$CHECK_TIME_DIFF"
         exit 1
       elif [[ $CHECK_TIME_DIFF_INT -ge $2 ]]
@@ -53,6 +55,7 @@ else
       else
         echo "OK - time is $CHECK_TIME_DIFF $FAST_SLOW_VALUE of NTP Time"
         echo "|Time Differences in=$CHECK_TIME_DIFF"
+        exit 0
       fi
   fi
 fi
